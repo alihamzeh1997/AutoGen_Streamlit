6accb7d276eacf2051dc32d02f5d50a860614c67
alihamzeh1997 <159444435+alihamzeh1997@users.noreply.github.com>
2024-11-13 11:52:57 +0000

    test w

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(page_title="Multi AI Agent App", page_icon="🤖")
-st.title("🤖 Multi AI Agent App test")
+st.title("🤖 Multi AI Agent App test 2")
 
 st.write(
     "In this web application you can create mutiple conversable AI agents to speak with each other in a chat."
