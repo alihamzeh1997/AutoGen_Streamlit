bb17775ee7fc0f5697397874aedfe33e8367c722
alihamzeh1997 <159444435+alihamzeh1997@users.noreply.github.com>
2024-11-13 11:48:39 +0000

    test commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+from autogen import GroupChatManager, ConversableAgent, GroupChat
 
-st.title("🎈 My new app")
+
+st.set_page_config(page_title="Multi AI Agent App", page_icon="🤖")
+st.title("🤖 Multi AI Agent App test")
+
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "In this web application you can create mutiple conversable AI agents to speak with each other in a chat."
 )
